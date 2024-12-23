d8cb58d477ddba6f3e1228b275bc1a79b593d7da
Alisha Barik <alisha@Alishas-MacBook-Air.local>
2024-12-24 00:06:24 +0530

    Change in app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import GameOver from "./components/GameOver";
 //import WINNING_COMBINATIONS  from "winning-combinations.js";
-
+const PLAYERS = {
+  X:"Player 1",
+  O:"Player 2"
+}
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -63,18 +66,8 @@ function derivedActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
-function App() {
-  const [gameTurns, setGameTurns] = useState([]);
-  // const [activePlayer, setActivePlayer] = useState("X");
-  const activePlayer = derivedActivePlayer(gameTurns);
-  let gameBoard = initialGameBoard;
-  for (const turn of gameTurns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-  let winner = null;
+function deriveWinner(gameBoard, players){
+  let winner;
   for (const combination of WINNING_COMBINATIONS) {
     const firstSquareSymbol =
       gameBoard[combination[0].row][combination[0].column];
@@ -88,9 +81,33 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      console.log("winner-->" + players)
+      winner = players[firstSquareSymbol];
     }
   }
+
+return winner;
+}
+
+function derivedGameBoard(gameTurns){
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+  return gameBoard;
+}
+function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  const [players, setPlayers] = useState(PLAYERS)
+  // const [activePlayer, setActivePlayer] = useState("X");
+  const activePlayer = derivedActivePlayer(gameTurns);
+  
+const gameBoard = derivedGameBoard(gameTurns)
+
+  const winner = deriveWinner(gameBoard, players);
 
   const hasDraw = gameTurns.length === 9 && !winner;
 
@@ -111,19 +128,31 @@ function App() {
   function handleRestart() {
     setGameTurns([]);
   }
+
+  function handlePlayerNameChange(symbol, newName){
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol] : newName
+      };
+    });
+
+  }
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
           <Player
-            initialName={"Alisha Bro"}
+            initialName={PLAYERS.X}
             symbol={"X"}
             isActive={activePlayer === "X"}
+            onChangeNames = {handlePlayerNameChange}
           />
           <Player
-            initialName={"Prabhat Bhai"}
+            initialName={PLAYERS.O}
             symbol={"O"}
             isActive={activePlayer === "O"}
+            onChangeNames= {handlePlayerNameChange}
           />
         </ol>
         {(winner || hasDraw) && (
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,21 +1,28 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({ initialName, symbol, isActive, onChangeNames }) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
-  let edititablePlayerName = <span className="player-name">{playerName}</span>;
-  if (isEditing) {
-    edititablePlayerName = (
-      <input type="text" required onChange={onChangeName} value={playerName} />
-    );
-  }
+
   function handleEditClick() {
     setIsEditing(!isEditing);
+    if(isEditing){
+      onChangeNames(symbol, playerName);
+    }
+    
   }
-  function onChangeName(event) {
+  function handleChange(event) {
     console.log("event->" + event);
     setPlayerName(event.target.value);
   }
+
+  let edititablePlayerName = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    edititablePlayerName = (
+      <input type="text" required onChange={handleChange} value={playerName} />
+    );
+  }
+
   return (
     <>
       <li className={isActive ? "active" : undefined}>
